2f60576bcb70498969d4f7d79f99a1c46fc822bc
Maaz <maaazsurti@gmail.com>
2023-03-07 15:28:27 +0300

    Update common_request_leave_details.dart

--- lib/views/RequestLeave/Components/common_request_leave_details.dart
+++ lib/views/RequestLeave/Components/common_request_leave_details.dart
@@ -3,6 +3,7 @@ import 'package:learning_flutter/reusable_views/handle_bar.dart';
 
 import '../../../constants/constants.dart';
 import '../../../helpers/colors.dart';
+import '../../../reusable_views/button.dart';
 
 class CommonRequestLeaveDetails extends StatefulWidget {
   const CommonRequestLeaveDetails({super.key});
@@ -15,68 +16,110 @@ class CommonRequestLeaveDetails extends StatefulWidget {
 class _CommonRequestLeaveDetailsState extends State<CommonRequestLeaveDetails> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: logicalHeight * 0.8,
-      child: Center(
-        child: Column(
-          children: [
-            Handlebar(),
-            SizedBox(
-              height: 24,
-            ),
-            Text(
-              "8",
-              style: TextStyle(
-                  fontFamily: fontName,
-                  fontSize: 32,
-                  height: 1,
-                  color: AppColor.primary,
-                  fontWeight: FontWeight.bold),
-            ),
-            Text(
-              "days remaining",
-              style: TextStyle(
-                  fontFamily: fontName,
-                  fontSize: 18,
-                  height: 1,
-                  color: Colors.black87,
-                  fontWeight: FontWeight.normal),
-            ),
-            SizedBox(height: 24),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 48),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "From:",
-                    style: TextStyle(
-                        fontFamily: fontName,
-                        fontSize: 14,
-                        height: 1,
-                        color: AppColor.primary,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 16),
-                  DateField(),
-                  SizedBox(height: 16),
-                  Text(
-                    "To:",
-                    style: TextStyle(
-                        fontFamily: fontName,
-                        fontSize: 14,
-                        height: 1,
-                        color: AppColor.primary,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(
-                    height: 16,
-                  ),
-                  DateField()
-                ],
+    return SingleChildScrollView(
+      child: SizedBox(
+        height: logicalHeight * 0.8,
+        child: Center(
+          child: Column(
+            children: [
+              Handlebar(),
+              SizedBox(
+                height: 24,
               ),
-            )
-          ],
+              Text(
+                "8",
+                style: TextStyle(
+                    fontFamily: fontName,
+                    fontSize: 32,
+                    height: 1,
+                    color: AppColor.primary,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "days remaining",
+                style: TextStyle(
+                    fontFamily: fontName,
+                    fontSize: 18,
+                    height: 1,
+                    color: Colors.black87,
+                    fontWeight: FontWeight.normal),
+              ),
+              SizedBox(height: 24),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 48),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "From:",
+                      style: TextStyle(
+                          fontFamily: fontName,
+                          fontSize: 14,
+                          height: 1,
+                          color: AppColor.primary,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 16),
+                    DateField(),
+                    SizedBox(height: 16),
+                    Text(
+                      "To:",
+                      style: TextStyle(
+                          fontFamily: fontName,
+                          fontSize: 14,
+                          height: 1,
+                          color: AppColor.primary,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    DateField(),
+                    SizedBox(
+                      height: 56,
+                    ),
+                    Text(
+                      "Want to leave a message?",
+                      style: TextStyle(
+                          fontFamily: fontName,
+                          fontSize: 19,
+                          height: 1,
+                          color: AppColor.primary,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 16),
+                    Container(
+                      height: 140,
+                      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 0),
+                      decoration: BoxDecoration(
+                          shape: BoxShape.rectangle,
+                          border: Border.all(
+                            color: AppColor.primary,
+                            width: 0.6,
+                          )),
+                      child: TextField(
+                        cursorColor: Colors.black54,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText:
+                              "Type your reason for requesting leave here...",
+                          hintStyle: TextStyle(
+                              fontFamily: fontName,
+                              fontSize: 11,
+                              color: Colors.black87),
+                        ),
+                        keyboardType: TextInputType.multiline,
+                        maxLines: null,
+                      ),
+                    ),
+                    SizedBox(height: 96),
+                    Button(title: "Submit", action: () {}),
+                    SizedBox(height: 36),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -94,7 +137,13 @@ class _DateFieldState extends State<DateField> {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: () {
+        showDatePicker(
+            context: context,
+            initialDate: DateTime.now(),
+            firstDate: DateTime.now(),
+            lastDate: DateTime(2024, 9, 7, 17, 30));
+      },
       child: Container(
         padding: EdgeInsets.symmetric(horizontal: 8, vertical: 6),
         color: AppColor.primary,
